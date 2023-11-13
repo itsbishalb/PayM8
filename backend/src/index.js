d64de386eaf5d0f111cdc2841f8b675afa833a7d
Bishal Bhandari <itsbishalb@gmail.com>
2023-11-13 01:51:03 +0000

    feat: Added Model for User

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,14 @@ const connectDB = require("./db/db");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
-
+const { authRouter } = require("./routes/auth");
 connectDB();
 
 app.get("/", (request, response) => {
     response.send("Hi there");
 });
+
+app.use("api/", authRouter);
 
 app.listen(PORT, () => {
     console.log("Listen on the port 8000...");
